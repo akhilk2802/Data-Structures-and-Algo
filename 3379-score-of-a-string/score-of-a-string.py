0825a1accbc67f7c5c67062f9cbbd4219d70dcba
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2024-06-01 18:32:43 +0530

    Time: 37 ms (52.22%) | Memory: 16.4 MB (98.12%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.py
+++ 3379-score-of-a-string/score-of-a-string.py
@@ -2,9 +2,6 @@ class Solution:
     def scoreOfString(self, s: str) -> int:
 
         total = 0
-        for i in range(len(s) - 1):
-            ascii1 = ord(s[i])
-            ascii2 = ord(s[i + 1])
-            diff = abs( ascii1 - ascii2 )
-            total += diff
+        for i in range(len(s) - 1): 
+            total += abs( ord(s[i]) - ord(s[i + 1]) )
         return total
