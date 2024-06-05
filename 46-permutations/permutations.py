262c3170affeefbd6437d88c4aa2fefdf735666d
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2024-06-05 19:23:44 +0530

    Time: 43 ms (40.04%) | Memory: 16.6 MB (94.47%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,15 @@
+class Solution:
+    ans = []
+    def recurPermute(self, index: int, nums: List[int], ans: List[List[int]]):
+        if index == len(nums):
+            ans.append(nums[:])
+            return
+        for i in range(index, len(nums)):
+            nums[index], nums[i] = nums[i], nums[index]
+            self.recurPermute(index + 1, nums, ans)
+            nums[index], nums[i] = nums[i], nums[index]
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.ans = []
+        self.recurPermute(0, nums, self.ans)
+        return self.ans
