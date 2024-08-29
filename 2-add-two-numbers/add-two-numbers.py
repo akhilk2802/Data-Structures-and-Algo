c746ed5b1f5b70437e21f28c8eae43e9c63762f6
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2024-08-29 23:29:09 +0530

    Time: 56 ms (48.87%) | Memory: 16.5 MB (73.98%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,6 +5,7 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
         head = ListNode(0)
         tail = head
         carry = 0
@@ -13,18 +14,21 @@ class Solution:
             d1 = l1.val if l1 is not None else 0
             d2 = l2.val if l2 is not None else 0
 
-            sum = d1 + d2 + carry
-            d = sum % 10
+            sum = d1 + d2 + carry 
             carry = sum // 10
+            d = sum % 10
 
             new = ListNode(d)
-            tail.next = new
+            tail.next = new 
             tail = tail.next
 
             l1 = l1.next if l1 is not None else None
             l2 = l2.next if l2 is not None else None
-
+        
         result = head.next
         head.next = None
         return result
+
+        
+
         
