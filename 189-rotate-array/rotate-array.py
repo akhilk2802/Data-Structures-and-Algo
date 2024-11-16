fffe23295b2ca09a52f32c212f4bcf0a1cbe05e9
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2024-11-16 14:34:14 -0500

    Time: 3 ms (76.52%) | Memory: 24.6 MB (77.79%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -4,9 +4,13 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        if n == 1 or k == 0 or k % n == 0:
-            return
         k = k % n
-        temp = nums[-k:]      
-        temp.extend(nums[:n-k])
-        nums[:] = temp
+        self.reverse(nums, 0, n-1)
+        self.reverse(nums, 0, k-1)
+        self.reverse(nums, k, n-1)
+
+    def reverse(self, arr, start, end):
+        while start < end:
+            arr[start], arr[end] = arr[end], arr[start]
+            start += 1
+            end -= 1
