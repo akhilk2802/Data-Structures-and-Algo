2913f46cd4b9c2b85153765639c30f4fe63c4f51
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2024-11-16 23:48:38 -0500

    Time: 0 ms (100.00%) | Memory: 18.4 MB (31.94%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,35 +1,23 @@
-def is_integer(s):
-    if not s:
-        return False
-    if s[0] in ('-', '+'):
-        return s[1:].isdigit()
-    return s.isdigit()
-    
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-
         if not tokens:
             return None
-        
-        stack = []
-        n = len(tokens)
 
-        for i in range(n):
-            if is_integer(tokens[i]):
-                stack.append(int(tokens[i]))
-            elif tokens[i] in "+/*-":
+        stack = []
+        o = {
+            "+": lambda a, b: a + b,
+            "-": lambda a, b: b - a,
+            "*": lambda a, b: a * b,
+            "/": lambda a, b: int(b/a)
+        }
+
+        for token in tokens:
+            try:
+                num = int(token)
+                stack.append(num)
+            except:
                 a = stack.pop()
                 b = stack.pop()
-                if tokens[i] == "+":
-                    c = a+b
-                    stack.append(c)
-                if tokens[i] == "-":
-                    c = b-a
-                    stack.append(c)
-                if tokens[i] == "*":
-                    c = a*b
-                    stack.append(c)
-                if tokens[i] == "/":
-                    c = int(b/a)
-                    stack.append(c)        
+                stack.append(o[token](a, b))
         return stack.pop()
+        
