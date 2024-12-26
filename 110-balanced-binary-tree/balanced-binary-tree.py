f146776b05e90e13662d436d5c72af5c0c93e689
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2024-12-26 15:14:19 -0500

    Time: 0 ms (100.00%) | Memory: 18.9 MB (9.31%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -8,23 +8,14 @@ class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         def checkHeightAndBalance(node):
             if not node:
-                return 0  # Height of an empty tree is 0
-
-            # Recursively get the height of left and right subtrees
+                return 0
             leftHeight = checkHeightAndBalance(node.left)
-            if leftHeight == -1:  # Left subtree is unbalanced
+            if leftHeight == -1:
                 return -1
-
             rightHeight = checkHeightAndBalance(node.right)
-            if rightHeight == -1:  # Right subtree is unbalanced
+            if rightHeight == -1:
                 return -1
-
-            # Check if the current node is balanced
             if abs(leftHeight - rightHeight) > 1:
-                return -1  # Current node is unbalanced
-
-            # Return the height of the current node
+                return -1
             return 1 + max(leftHeight, rightHeight)
-
-        # If the function returns -1, the tree is unbalanced
         return checkHeightAndBalance(root) != -1
