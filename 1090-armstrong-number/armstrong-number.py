c9c739ae4ecaf4d9d7bb6cff91d43a161c24718f
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-01-13 20:34:03 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (40.07%) - LeetSync

--- 1090-armstrong-number/armstrong-number.py
+++ 1090-armstrong-number/armstrong-number.py
@@ -3,8 +3,6 @@ class Solution:
 
         digits = str(n)
         num_digits = len(digits)
-
         armstrong_sum = sum(int(digit) ** num_digits for digit in digits)
 
         return armstrong_sum == n
-    
