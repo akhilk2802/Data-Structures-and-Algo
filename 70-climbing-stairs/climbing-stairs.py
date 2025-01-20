df36fc5167c5af834c5c4232946eb2d6a42a2ffb
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-01-19 20:28:31 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (55.85%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -4,12 +4,18 @@ class Solution:
         if n <= 2:
             return n
         
-        dp = [0] * (n+1)
-        dp[0] = 0
-        dp[1] = 1
-        dp[2] = 2
+        # dp = [0] * (n+1)
+        # dp[0] = 0
+        # dp[1] = 1
+        # dp[2] = 2
+
+        prev = 1
+        prev2 = 2
 
         for i in range(3, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
+            # dp[i] = dp[i-1] + dp[i-2]
+            curi = prev2 + prev
+            prev = prev2
+            prev2 = curi
 
-        return dp[n]
+        return prev2
