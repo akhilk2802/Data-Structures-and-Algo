099fa0e109eba5c6cc384c0df7160550812bc427
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-01-29 14:47:17 -0500

    Time: 4171 ms (61.68%) | Memory: 17.8 MB (65.00%) - LeetSync

---
+++ 79-word-search/word-search.py
@@ -0,0 +1,31 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+
+        startChar = word[0]
+        n = len(word)
+
+        rows, cols = len(board), len(board[0])
+        dir = [(1, 0), (0, 1), (0, -1), (-1, 0)]
+        visited = set()
+
+        def dfs(row, col, visited, index):
+            if index == n-1:
+                return True
+            visited.add((row, col))
+            for dr, dc in dir:
+                nr, nc = dr + row, dc + col
+                if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in visited:
+                    if board[nr][nc] == word[index + 1]:
+                        if dfs(nr, nc, visited, index + 1):
+                            return True
+            visited.remove((row, col))
+            return False
+        
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == startChar:
+                    visited = set()
+                    if dfs(i, j, visited, 0):
+                        return True
+
+        return False
