e10afb775cb83f6fb7ddec4c72fc82472b136eb7
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-02-18 09:20:15 -0500

    Time: 7 ms (15.17%) | Memory: 18.6 MB (94.54%) - LeetSync

---
+++ 979-di-string-match/di-string-match.py
@@ -0,0 +1,24 @@
+class Solution:
+    def diStringMatch(self, s: str) -> List[int]:
+        
+        n = len(s)
+        l = 0
+        r = n
+        perm = []
+
+        for i in range(n+1):
+            if i == n:
+                if s[i-1] == "I":
+                    perm.append(r)
+                if s[i-1] == "D":
+                    perm.append(l)
+                print("S : ", perm)
+                return perm
+            if s[i] == "I":
+                perm.append(l)
+                l += 1
+            if s[i] == "D":
+                perm.append(r)
+                r -= 1
+        
+        
