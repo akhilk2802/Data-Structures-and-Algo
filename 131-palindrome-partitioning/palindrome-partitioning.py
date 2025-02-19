8f895cc12bc2939e690a46660fd3110552f0f129
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-02-19 09:52:27 -0500

    Time: 55 ms (39.79%) | Memory: 32.7 MB (90.33%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,28 +1,32 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        
-        res = []
-        path = []
 
-        def partitionHelper(index):
-            if index == len(s):
-                res.append(path[:])
-                return 
-            for i in range(index, len(s)):
-                if isPalindrome(s, index, i):
-                    path.append(s[index:i+1])
-                    partitionHelper(i+1)
+        # Algo - 
+        # generate a substring using recursion
+        # generate a substring and check if it is a valid palindrome
+        # if it is then add it to the result array
+        
+        def isPalindrome(subString):
+            l, r = 0, len(subString)-1
+            while l <= r:
+                if subString[l] == subString[r]:
+                    l += 1
+                    r -= 1
+                else:
+                    return False
+            return True
+        
+        def generateSubString(start, path, result, s):
+            if start == len(s):
+                result.append(path[:])
+
+            for end in range(start + 1, len(s) + 1):
+                sub = s[start:end]
+                if isPalindrome(sub):
+                    path.append(sub)
+                    generateSubString(end, path, result, s)
                     path.pop()
 
-        def isPalindrome(s, start, end):
-            while start <= end:
-                if s[start] != s[end]:
-                    return False
-                start += 1
-                end -= 1
-            return True
-
-        partitionHelper(0)
-        return res
-
-        
+        result = []
+        generateSubString(0, [], result, s)
+        return result
