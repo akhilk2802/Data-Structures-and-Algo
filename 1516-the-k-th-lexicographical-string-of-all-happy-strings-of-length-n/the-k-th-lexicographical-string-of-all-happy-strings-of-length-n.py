c11dfa6e1f998b303201175b3c27ad1e445ee18c
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-02-19 09:26:30 -0500

    Time: 3 ms (82.28%) | Memory: 17.7 MB (94.09%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -8,23 +8,32 @@ class Solution:
         # algo improve - 
         # 1. Generate the happy string untill i reach kth element 
         # 2. Once reached return the string
-        # 3. No need to store the strings as well 
+        # 3. No need to store the strings as well
+
         chars = {"a", "b", "c"}
+        sChars = sorted(chars)
         happy = []
+        count = 0
 
         def generate_strings(chars, n, current=""):
+            nonlocal count
             if len(current) == n:
-                happy.append(current[:])
+                count += 1
+                if count == k:
+                    happy.append(current[:])
                 return 
 
-            for ch in chars:
+            for ch in sChars:
                 if not current or current[-1] != ch:
                     generate_strings(chars, n, current + ch)
+                    if happy:
+                        return
 
         generate_strings(chars, n)
-        happy.sort()
+        # happy.sort()
 
-        if len(happy) >= k:
-            return happy[k-1]
+        # if len(happy) >= k:
+        #     return happy[k-1]
 
-        return ""
+        # return ""
+        return happy[0] if happy else ""
