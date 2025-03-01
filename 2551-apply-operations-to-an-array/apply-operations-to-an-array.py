124ac4200ddcbf57cec951702c3dbf818ebdf96f
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-03-01 10:35:48 -0500

    Time: 3 ms (34.56%) | Memory: 17.8 MB (94.08%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -0,0 +1,33 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        
+        # Algo -> 
+        # 1. one pass
+        # 2. keep updating the array with the operations 
+        # 3. perform swap operation to put all the zeros to the end 
+        # 4. will have to use two for loops
+
+        n = len(nums)
+        for i in range(n - 1):
+            if nums[i] == nums[i + 1]:
+                nums[i] = 2 * nums[i]
+                nums[i + 1] = 0
+        
+        print("nums: ",  nums)
+
+        l = 0
+        r = l + 1
+        
+        while r < n:
+            if nums[r] > 0 and nums[l] == 0:
+                temp = nums[l]
+                nums[l] = nums[r]
+                nums[r] = temp
+                l += 1
+            elif nums[l] > 0:
+                l += 1
+            r += 1
+
+        print("post swap : ", nums)
+        return nums
+            
