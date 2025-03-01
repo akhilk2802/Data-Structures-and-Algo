29a0e114d1e070eb380a0c7c2395ae3c06b3cd0a
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-03-01 15:07:04 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (86.80%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,22 +1,29 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        def rob_linear(houses):
-            prev2 = 0
-            prev = 0
-            for house in houses:
-                current = max(prev, prev2 + house)
-                prev2 = prev
-                prev = current
-            return prev
-        
-        n = len(nums)
+        def robHouse(arr):
+            n = len(arr)
 
-        if n == 1: return nums[0]
-        if n == 2: return max(nums[0], nums[1])
+            if n == 0:
+                return 0
+            if n == 1:
+                return arr[0]
+            if n == 2:
+                return max(arr[0], arr[1])
+            
+            dp = [0] * n
+            dp[0] = arr[0]
+            dp[1] = max(arr[0], arr[1])
+            # dp[2] = max(arr[0] + arr[2], arr[1])
 
-        case1 = rob_linear(nums[1:])
-        case2 = rob_linear(nums[:-1])
+            for i in range(2, n):
+                dp[i] = max(dp[i-1], dp[i-2] + arr[i])
 
-        return max(case1, case2)
+            return dp[n-1]
 
+    
+        if len(nums) == 1: return nums[0]
+        ans1 = robHouse(nums[1:])
+        ans2 = robHouse(nums[:-1])
+
+        return max(ans1, ans2)
