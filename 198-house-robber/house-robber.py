6a7f3d17cfade7ec3e77cfa03a61dc59f3f07dd9
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-03-01 14:52:09 -0500

    Time: 3 ms (7.75%) | Memory: 17.8 MB (34.00%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,14 +1,26 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
 
-        if len(nums) == 0: return 0
-        if len(nums) == 1: return nums[0]
-        if len(nums) == 2: return max(nums[0], nums[1])
-        
-        dp = [0] * len(nums)
+        # algo - 
+        # 1. keep a track of total value after stealing each house in their respected order 
+        # 2. return the max of last two elements in a dp array 
+        # 3. this should be totally based on the number of houses
+
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums[0], nums[1])
+        if n == 3: 
+            return max(nums[0] + nums[2], nums[1])
+
+        dp = [0] * n
         dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i-1], nums[i]+dp[i-2])
+        dp[1] = max(nums[1], nums[0])
         
-        return dp[-1]
+        for i in range(2, n):
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+
+        print("dp : ", dp)
+
+        return dp[n-1]
