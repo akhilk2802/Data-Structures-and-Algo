c3a7b00d252fe6e545ec5f0a83a1207cd5998f01
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-03-02 09:39:52 -0500

    Time: 236 ms (90.17%) | Memory: 19.2 MB (63.67%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,18 +1,26 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
 
-        total_sum = sum(nums)
-        if total_sum % 2 != 0:
+        # 1, 5, 11, 5
+        # sum = 22
+        # total = sum // 2
+        # algo - 
+        # basically i need to find a subset whose sum is 11
+        # if thats not possible, return false
+
+        if sum(nums) % 2:
             return False
-        
-        target = total_sum // 2
 
-        dp = [False] * (target + 1)
-        dp[0] = True
+        dp = set()
+        dp.add(0)
+        target = sum(nums) // 2
 
-        for num in nums:
-            for i in range(target, num-1, -1):
-                dp[i] = dp[i] or dp[i-num]
-
-        return dp[target]
-        
+        for i in range(len(nums)):
+            nextDP = set()
+            for t in dp:
+                if (t + nums[i]) == target:
+                    return True
+                nextDP.add(t + nums[i])
+                nextDP.add(t)
+            dp = nextDP
+        return True if target in dp else False
