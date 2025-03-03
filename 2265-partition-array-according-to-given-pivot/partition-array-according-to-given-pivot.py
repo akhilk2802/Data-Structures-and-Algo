af97e20cd3ad767805ff75db031536c33b3ab0db
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-03-03 08:57:56 -0500

    Time: 49 ms (25.26%) | Memory: 34.9 MB (23.77%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -15,7 +15,7 @@ class Solution:
         result = []
         for num in l:
             result.append(num)
-        
+
         for i in range(len(nums) - (len(l) + len(r))):
             result.append(pivot)
 
@@ -24,3 +24,24 @@ class Solution:
 
         # print("Number is -> ", result)
         return result
+
+
+        # l, r = 0, 1
+        
+
+        # while r < len(nums):
+            
+        #     # if left pointers value is greater than right pointers value, swap them untill i reach the pivot
+        #     if nums[l] > nums[r]:
+        #         nums[l], nums[r] = nums[r], nums[l]
+                
+        #         l += 1
+        #         r += 1
+        #     # once i reach the pivot element, i want to stop moving the left pointer and keep it on the pivot element and move only the right pointer, where ever i find the element less than or equal to pivot element, i will add it to the left pointer position and increase the left pointer to next (+1), once i hit the right pointer to the end of the list i will return the array 
+        #     if nums[l] == pivot:
+
+
+
+
+
+                
