f3ec04646fa68991b1ae059df0d89796bcbf1bd0
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-03-04 14:55:15 -0500

    Time: 7 ms (57.79%) | Memory: 19.1 MB (43.62%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -2,23 +2,30 @@ class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
 
         n = len(isConnected)
-        v = set()
+        adj_list = defaultdict(list)
+
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j] == 1:
+                    adj_list[i].append(j)
+
+        # print("Adjacency list : ", adj_list)
+        visited = set()
+        count = 0 
 
         def dfs(node):
-            for neighbor in range(n):
-                print("neighbor :", neighbor)
-                if isConnected[node][neighbor] == 1 and neighbor not in v:
-                    v.add(neighbor)
+            visited.add(node)
+
+            for neighbor in adj_list[node]:
+                if neighbor not in visited:
                     dfs(neighbor)
 
-        count = 0
-
-        for city in range(n):
-            if city not in v:
+        for node in range(n):
+            if node not in visited:
+                dfs(node)
                 count += 1
-                v.add(city)
-                dfs(city)
 
+        print("count : ", count)
         return count
 
-        
+
