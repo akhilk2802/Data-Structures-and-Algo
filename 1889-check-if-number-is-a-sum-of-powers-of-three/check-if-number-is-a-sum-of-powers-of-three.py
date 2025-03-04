2bc13bc0418f02d18beb0c2d24b0f2f6ec8957e0
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-03-04 09:47:56 -0500

    Time: 4 ms (10.76%) | Memory: 17.9 MB (18.33%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,17 +1,25 @@
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
 
+        # time and space complexity - 
+        # 1. since i am diving n by 3 at every iteration - its going to be O(log3 n) -> this turns out to O(log n)
+        # 2. space complexity -> O(1)
 
-        while n != 1:
-            # print("n: ", n)
-            mod = n % 3
-            if mod > 1:
-                return False
+        # while n != 1:
+        #     # print("n: ", n)
+        #     mod = n % 3
+        #     if mod > 1:
+        #         return False
+        #     div = n // 3
+        #     if div == 0:
+        #         return False
+        #     n = div
 
-            div = n // 3
-            if div == 0:
-                return False
+        # return True
 
-            n = div
+        if n == 1: return True
+        if n % 3 > 1: return False
+        if n // 3 == 0: return False
 
-        return True
+        return self.checkPowersOfThree(n // 3)
+
