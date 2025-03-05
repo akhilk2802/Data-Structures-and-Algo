ea7d9fd7e3c433de46df2058699d968de255c236
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-03-05 09:46:35 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (37.52%) - LeetSync

---
+++ 269-alien-dictionary/alien-dictionary.py
@@ -0,0 +1,70 @@
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+
+        adj_list = defaultdict(list)
+
+        for word in words:
+            for char in word:
+                if char not in adj_list:
+                    adj_list[char] = []
+
+        def diff_character(word1, word2):
+            n1 = len(word1)
+            n2 = len(word2)
+            i, j = 0, 0
+
+            while i < n1 and j < n2:
+                if word1[i] == word2[j]:
+                    i += 1
+                    j += 1
+                else: 
+                    # print([word2[j], word1[i]])
+                    return [word1[i], word2[j]]
+            
+            return None
+
+        n = len(words)
+        indegree = defaultdict(int)
+        for i in range(1, n):
+            current_word = words[i]
+            prev_word = words[i-1]
+
+            if len(prev_word) > len(current_word) and prev_word.startswith(current_word):
+                return ""
+
+            edge = diff_character(current_word, prev_word)
+            # print("Node 1 and Node 2: ", node1, node2)
+            # indegree[node2] += 1
+            # adj_list[node1].append(node2)
+            if edge:
+                node1, node2 = edge
+
+                if node2 not in adj_list[node1]:
+                    adj_list[node1].append(node2)
+                    indegree[node2] += 1
+
+        # print("ADJ : ", adj_list)
+
+        queue = deque()
+        topo = []
+
+        # print(indegree)
+
+        for char in adj_list:
+            if indegree[char] == 0:
+                queue.append(char)
+
+        while queue:
+            node = queue.popleft()
+            topo.append(node)
+
+            for neighbor in adj_list[node]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+
+        # print("topo : ", topo)
+        if len(topo) != len(adj_list):
+            return ""
+            
+        return "".join(topo[::-1])
