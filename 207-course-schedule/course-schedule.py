39eab61f31577c994f884e703c5902463585d623
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-03-04 22:29:02 -0500

    Time: 3 ms (89.08%) | Memory: 19 MB (73.42%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,30 +1,60 @@
 class Solution:
     def canFinish(self, n: int, prerequisites: List[List[int]]) -> bool:
         
+        # adj = defaultdict(list)
+        # for u, v in prerequisites:
+        #     adj[u].append(v)
+        # print("ADj :", adj)
+
+        # visited = [0] * n
+
+        # def dfs(node):
+        #     if visited[node] == 1:
+        #         return False
+        #     if visited[node] == 2:
+        #         return True
+
+        #     visited[node] = 1
+
+        #     for neighbor in adj[node]:
+        #         if not dfs(neighbor):
+        #             return False
+
+        #     visited[node] = 2
+        #     return True
+
+        # for course in range(n):
+        #     if visited[course] == 0:
+        #         if not dfs(course):
+        #             return False
+
+        # return True
+
         adj = defaultdict(list)
-        for u, v in prerequisites:
-            adj[u].append(v)
+        indegree = [0] * n
+        for course, prereq in prerequisites:
+            indegree[course] += 1
+            adj[prereq].append(course)
 
-        visited = [0] * n
+        # print("ADJ : ", adj)
+        # print("IN : ", indegree)
 
-        def dfs(node):
-            if visited[node] == 1:
-                return False
-            if visited[node] == 2:
-                return True
+        q = deque()
+        for i in range(n):
+            if indegree[i] == 0:
+                q.append(i)
+        
+        topo = []
 
-            visited[node] = 1
+        while q:
+            node = q.popleft()
 
+            topo.append(node)
             for neighbor in adj[node]:
-                if not dfs(neighbor):
-                    return False
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    q.append(neighbor)
 
-            visited[node] = 2
-            return True
+        # print("TOPO : ", topo)
 
-        for course in range(n):
-            if visited[course] == 0:
-                if not dfs(course):
-                    return False
-
-        return True
+        return len(topo) == n
