77a623d5977e1f600f819ce71537583f211cd548
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-03-10 15:43:12 -0400

    Time: 42 ms (75.70%) | Memory: 42.1 MB (57.02%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -1,25 +1,40 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
 
-        positive_arr = []
-        negative_arr = []
+        positive_index = 0
+        negative_index = 1
+        result = [0] * (len(nums))
 
         for i in range(len(nums)):
             if nums[i] > 0:
-                positive_arr.append(nums[i])
-            elif nums[i] < 0:
-                negative_arr.append(nums[i])
+                result[positive_index] = nums[i]
+                positive_index += 2
+                continue
+            result[negative_index] = nums[i]
+            negative_index += 2
 
-        result = []
-        i, j = 0, 0
+        # print("result : ", result)
+        return result
 
-        while i < len(positive_arr) and j < len(negative_arr):
-            result.append(positive_arr[i])
-            result.append(negative_arr[j])
-            i+=1
-            j+=1
+        # positive_arr = []
+        # negative_arr = []
+
+        # for i in range(len(nums)):
+        #     if nums[i] > 0:
+        #         positive_arr.append(nums[i])
+        #     elif nums[i] < 0:
+        #         negative_arr.append(nums[i])
+
+        # result = []
+        # i, j = 0, 0
+
+        # while i < len(positive_arr) and j < len(negative_arr):
+        #     result.append(positive_arr[i])
+        #     result.append(negative_arr[j])
+        #     i+=1
+        #     j+=1
             
 
-        # print("result: ", result)
-        return result
+        # # print("result: ", result)
+        # return result
         
