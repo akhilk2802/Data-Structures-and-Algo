86ab4f280ea859e71c4c45f99d80b3d6664c9e13
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-03-12 15:53:23 -0400

    Time: 0 ms (100.00%) | Memory: 24.6 MB (96.28%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,19 +1,36 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         
-        low = 0
-        high = len(nums) - 1
 
-        while low < high:
-            mid = (low + high) // 2
+        '''
+        brute force =>
+        use hashmap , return the one with only one element 
 
+        optimised => 
+        binary search =>
+        [1,1,2,3,3,4,4,8,8]
+        [0,1,2,3,4,5,6,7,9]
+
+        [3,3,7,7,10,11,11]
+        [0,1,2,3,4 ,5 ,6 ]
+
+        if all the elements are in pairs from the start
+        find the mid, if the mid is odd and the second element is on odd we know the element is on the left
+        else the element is on the right 
+        now based on this observation we can manipulate the low and high of the search algorithm
+        '''
+
+        l, r = 0, len(nums) - 1
+
+        while l < r:
+
+            mid = (l + r) // 2
+            
             if mid % 2 == 1:
                 mid -= 1
-            if nums[mid] != nums[mid+1]:
-                high = mid
+            if nums[mid] != nums[mid + 1]:
+                r = mid
             else:
-                low = mid + 2
-        
-        return nums[low]
+                l = mid + 2
 
-        
+        return nums[l]
