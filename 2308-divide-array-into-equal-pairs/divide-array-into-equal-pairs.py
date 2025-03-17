ac53244fa2239cafb8f5884c8d5baabcaeb9772b
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-03-17 10:18:55 -0400

    Time: 2 ms (77.39%) | Memory: 17.8 MB (91.41%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.py
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.py
@@ -8,20 +8,20 @@ class Solution:
         keep increasing the count if two values are equal 
         '''
 
-        count = Counter(nums)
-        for c in count.values():
-            if c % 2 != 0:
-                return False
-        return True
-
-        # n = len(nums) // 2
-        # nums.sort()
-        # count = 0
-
-        # for i in range(1, len(nums), 2):
-        #     if nums[i] == nums[i-1]:
-        #         count += 1
-        #     else:
+        # count = Counter(nums)
+        # for c in count.values():
+        #     if c % 2 != 0:
         #         return False
+        # return True
 
-        # return count == n
+        n = len(nums) // 2
+        nums.sort()
+        count = 0
+
+        for i in range(1, len(nums), 2):
+            if nums[i] == nums[i-1]:
+                count += 1
+            else:
+                return False
+
+        return count == n
