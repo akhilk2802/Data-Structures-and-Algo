4359cfccfe2ac69452561ccc9a7ab597093aef03
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-03-17 10:16:10 -0400

    Time: 3 ms (70.86%) | Memory: 18.1 MB (24.91%) - LeetSync

---
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.py
@@ -0,0 +1,25 @@
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        
+        '''
+        Brute force approach -> sort things out 
+        maintain the count
+        as you go with the for loop after sorting
+        keep increasing the count if two values are equal 
+
+        '''
+
+        n = len(nums) // 2
+        nums.sort()
+        count = 0
+
+        for i in range(1, len(nums), 2):
+            if nums[i] == nums[i-1]:
+                count += 1
+            else:
+                return False
+
+        return count == n
+            
+        
+
