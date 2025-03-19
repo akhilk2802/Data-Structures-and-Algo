f711809d6d0b7b37890ee655636fb40b956abe07
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-03-19 14:50:24 -0400

    Time: 4 ms (30.54%) | Memory: 17.9 MB (37.73%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
@@ -0,0 +1,33 @@
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        '''
+        i want to understand the question first and then see what i can do right 
+        "", "(" + A + ")" 
+        1. my questions are, do i have to be worried only about () or {} or any other parantheses 
+        2. since you mentioned it is a string, does it only have parantheses or even integers or characters right
+        3. wha would be the length of the string 
+
+        usually if there is a string and a parantheses kind of problem, my intuition says its gonna be a stack based problem right 
+        correct me if i am wrong
+        now lets start with the brute force solution 
+
+        "(()())(())"
+
+        array or something like that 
+        since the out put is a string 
+        
+        ")"
+
+        stack = [(]
+        result = "()()()()(())"
+        '''
+
+        openP, result = 0, []
+        for char in s:
+            if char == ")":
+                openP -= 1
+            if openP > 0:
+                result.append(char)
+            if char == "(":
+                openP += 1
+        return ''.join(result)
