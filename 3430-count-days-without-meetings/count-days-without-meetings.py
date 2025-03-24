30124b849ecc2da5d2f12dba394a517fab375d71
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-03-23 21:22:02 -0400

    Time: 132 ms (80.35%) | Memory: 52.9 MB (30.73%) - LeetSync

---
+++ 3430-count-days-without-meetings/count-days-without-meetings.py
@@ -0,0 +1,47 @@
+class Solution:
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        
+        '''
+        lets optimise this ->
+        sort this based on the meeting start 
+        merge the intervals if there's an overlap
+        '''
+
+        meetings.sort(key=lambda x: x[0])
+        merged = []
+
+        for meeting in meetings:
+            meeting_start, meeting_end = meeting
+            if not merged or merged[-1][1] < meeting_start:
+                merged.append(meeting)
+            else:
+                merged[-1][1] = max(merged[-1][1], meeting_end)
+
+        # print("merged", merged)
+        total_days = 0
+
+        for meeting in merged:
+            meeting_start, meeting_end = meeting
+            total_days += (meeting_end - meeting_start + 1)
+        # print("total_days : ", total_days)
+
+        return days - total_days
+
+        
+
+        # Dumbest solution -> Vibe Coding lol
+        # meetings.sort(key=lambda x: x[0])
+        # meet = [False] * (days + 1)
+        # print("meet : ", meetings)
+
+        # for m in meetings:
+        #     m_start, m_end = m
+        #     for i in range(m_start, m_end + 1):
+        #         meet[i] = True
+
+        # print("meet -> ", meet)
+        # count = 0
+        # for i in range(1, days+1):
+        #     if meet[i] == False:
+        #         count += 1
+        # print("count => ", count)
