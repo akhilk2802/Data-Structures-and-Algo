1a4bb3665517d70c8f9423a45bf51a2fd1eaac87
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-03-25 12:48:44 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (37.83%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,12 +1,14 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
 
-        dp = [[0] * (i + 1) for i in range(numRows)]
+        dp = [[0] * i for i in range(1, numRows + 1)]
+
         for i in range(numRows):
             dp[i][0] = 1
             dp[i][i] = 1
 
             for j in range(1, i):
                 dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
-
+        
+        # print("dp : ", dp)
         return dp
