2f7bfc0fcd68de61f0ae7411f1f8d8cfe3011f2e
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-03-25 19:42:20 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (18.98%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
@@ -7,8 +7,8 @@ class Solution:
         for i in range(len(pattern) + 1):
             stack.append(i + 1)
 
-            if i == len(pattern) or pattern[i] == "I":
-                while stack: 
+            if len(pattern) == i or pattern[i] == "I":
+                while stack:
                     result.append(str(stack.pop()))
-        
+
         return "".join(result)
