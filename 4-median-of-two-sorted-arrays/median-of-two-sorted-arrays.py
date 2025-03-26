c26c2b7dec264ecda43f507478e694e30f9293a5
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-03-26 08:01:34 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (84.02%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,52 +1,63 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         
-        n1 = len(nums1)
-        n2 = len(nums2)
-        n3 = n1 + n2
+        
+        combined = nums1 + nums2
+        combined.sort()
 
-        ind2 = n3 // 2
-        ind1 = ind2 - 1
-        count, ind1el, ind2el = 0, -1, -1
+        n = len(combined)
 
-        i, j = 0, 0
-        while i < n1 and j < n2:
-            if nums1[i] < nums2[j]:
-                if count == ind1:
-                    ind1el = nums1[i]
-                if count == ind2:
-                    ind2el = nums1[i]
-                count += 1
-                i += 1
-            else:
-                if count == ind1:
-                    ind1el = nums2[j]
-                if count == ind2:
-                    ind2el = nums2[j]
-                count += 1
-                j += 1
+        if n % 2 == 1:
+            return combined[n // 2]
+        else:
+            return (combined[n // 2 - 1] + combined[n // 2]) / 2.0
 
-        # Process remaining elements in nums1 (if nums2 is exhausted)
-        while i < n1:
-            if count == ind1:
-                ind1el = nums1[i]
-            if count == ind2:
-                ind2el = nums1[i]
-            count += 1
-            i += 1
+        
+        # n1 = len(nums1)
+        # n2 = len(nums2)
+        # n3 = n1 + n2
 
-        # Process remaining elements in nums2 (if nums1 is exhausted)
-        while j < n2:
-            if count == ind1:
-                ind1el = nums2[j]
-            if count == ind2:
-                ind2el = nums2[j]
-            count += 1
-            j += 1
+        # ind2 = n3 // 2
+        # ind1 = ind2 - 1
+        # count, ind1el, ind2el = 0, -1, -1
 
-        # If the total number of elements is odd, return the middle element
-        if n3 % 2 == 1:
-            return float(ind2el)
-        # If the total number of elements is even, return the average of the two middle elements
-        return (ind1el + ind2el) / 2.0
-    
+        # i, j = 0, 0
+        # while i < n1 and j < n2:
+        #     if nums1[i] < nums2[j]:
+        #         if count == ind1:
+        #             ind1el = nums1[i]
+        #         if count == ind2:
+        #             ind2el = nums1[i]
+        #         count += 1
+        #         i += 1
+        #     else:
+        #         if count == ind1:
+        #             ind1el = nums2[j]
+        #         if count == ind2:
+        #             ind2el = nums2[j]
+        #         count += 1
+        #         j += 1
+
+        # # Process remaining elements in nums1 (if nums2 is exhausted)
+        # while i < n1:
+        #     if count == ind1:
+        #         ind1el = nums1[i]
+        #     if count == ind2:
+        #         ind2el = nums1[i]
+        #     count += 1
+        #     i += 1
+
+        # # Process remaining elements in nums2 (if nums1 is exhausted)
+        # while j < n2:
+        #     if count == ind1:
+        #         ind1el = nums2[j]
+        #     if count == ind2:
+        #         ind2el = nums2[j]
+        #     count += 1
+        #     j += 1
+
+        # # If the total number of elements is odd, return the middle element
+        # if n3 % 2 == 1:
+        #     return float(ind2el)
+        # # If the total number of elements is even, return the average of the two middle elements
+        # return (ind1el + ind2el) / 2.0
