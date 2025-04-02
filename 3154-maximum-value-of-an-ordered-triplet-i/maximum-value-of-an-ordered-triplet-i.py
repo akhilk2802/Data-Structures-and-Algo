c0e6d11760835d3f49651ecbd8b7eed858d423d1
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-04-02 09:20:25 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (54.86%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
@@ -1,5 +1,21 @@
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
+
+        '''
+        optimising further ->
+        there's an observation to make here ->
+        we need to maximize the left value and multiplication value
+
+        '''
+
+        left, diff, result = 0, 0, 0
+
+        for n in nums:
+            result = max(result, diff * n)
+            diff = max(diff, left - n)
+            left = max(left, n)
+
+        return result
         
         '''
         optimised ->
@@ -8,18 +24,18 @@ class Solution:
         if we want to maximise the value, num[i] should be as maximum then required
         '''
 
-        max_val = 0
-        n = len(nums)
+        # max_val = 0
+        # n = len(nums)
 
-        left = nums[0]
-        for j in range(1, n):
-            if nums[j] > left:
-                left = nums[j]
-            for k in range(j+1, n):
-                val = (left - nums[j]) * nums[k]
-                max_val = max(max_val, val)
+        # left = nums[0]
+        # for j in range(1, n):
+        #     if nums[j] > left:
+        #         left = nums[j]
+        #     for k in range(j+1, n):
+        #         val = (left - nums[j]) * nums[k]
+        #         max_val = max(max_val, val)
 
-        return max_val
+        # return max_val
 
 
 
