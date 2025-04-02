e09cc5553df572b755af9d42cb83ac66978e35a7
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-04-02 09:15:04 -0400

    Time: 7 ms (65.59%) | Memory: 18 MB (19.20%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
@@ -2,20 +2,42 @@ class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
         
         '''
+        optimised ->
+        greedy solution -> 
+        there's an observation to make here ->
+        if we want to maximise the value, num[i] should be as maximum then required
+        '''
+
+        max_val = 0
+        n = len(nums)
+
+        left = nums[0]
+        for j in range(1, n):
+            if nums[j] > left:
+                left = nums[j]
+            for k in range(j+1, n):
+                val = (left - nums[j]) * nums[k]
+                max_val = max(max_val, val)
+
+        return max_val
+
+
+
+        '''
         since the constraints are the way they are given, this can be solved with triple nested loops
         '''
 
-        max_val = float("-inf")
-        n = len(nums)
+        # max_val = float("-inf")
+        # n = len(nums)
 
-        for i in range(n - 2):
-            for j in range(i+1, n - 1):
-                for k in range(j+1, n):
-                    val = ((nums[i] - nums[j]) * nums[k])
-                    max_val = max(max_val, val)
+        # for i in range(n - 2):
+        #     for j in range(i+1, n - 1):
+        #         for k in range(j+1, n):
+        #             val = ((nums[i] - nums[j]) * nums[k])
+        #             max_val = max(max_val, val)
 
-        if max_val < 0:
-            return 0
+        # if max_val < 0:
+        #     return 0
 
-        return max_val
+        # return max_val
 
