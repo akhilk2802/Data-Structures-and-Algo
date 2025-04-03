6ef0805b78235322f207ca9bff63da243031659e
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-04-03 11:12:37 -0400

    Time: 7 ms (99.19%) | Memory: 20.3 MB (39.13%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -3,6 +3,7 @@ class Solution:
 
         m, n = len(grid), len(grid[0])
         dp = [[0 for _ in range(n)] for _ in range(m)]
+        # print("DP -> ", dp)
 
         dp[0][0] = grid[0][0]
 
@@ -12,9 +13,11 @@ class Solution:
         for i in range(1, n):
             dp[0][i] = dp[0][i-1] + grid[0][i]
 
-        # print("DP array : ", dp)
+        # print("DP after -> ", dp)
+
         for i in range(1, m):
             for j in range(1, n):
-                dp[i][j] = min((dp[i][j-1] + grid[i][j]), (dp[i-1][j] + grid[i][j]))
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
 
+        # print("DP -> ", dp)
         return dp[m-1][n-1]
