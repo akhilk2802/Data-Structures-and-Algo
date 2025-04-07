3c4fb33a30f1e83538223f27c24793a9f42820b6
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-04-07 12:25:12 -0400

    Time: 1 ms (39.93%) | Memory: 17.7 MB (89.69%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,17 +1,31 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+
+        n = len(s)
+        dp = [0] * (n + 1)
+
+        dp[n] = 1
+
+        for i in range(n - 1, -1, -1):
+            if s[i] == '0':
+                dp[i] = 0
+            else:
+                dp[i] = dp[i + 1]
+                if i + 1 < n and 10 <= int(s[i:i+2]) <= 26:
+                    dp[i] += dp[i + 2]
         
-        dp = [0 for _ in range(len(s) + 1)]
-        
-        dp[0] = 1
-        dp[1] = 0 if s[0] == "0" else 1
+        return dp[0]
 
-        for i in range(2, len(dp)):
-            if s[i-1] != "0":
-                dp[i] = dp[i-1]
+        # n = len(s)
+        # dp = [0] * (n + 1)
 
-            two_digit = int(s[i-2 : i])
-            if two_digit >= 10 and two_digit <= 26:
-                dp[i] += dp[i-2]
+        # dp[n] = 1
+        # for i in range(n-1, -1, -1):
+        #     if s[i] == '0':
+        #         dp[i] = 0
+        #     else:
+        #         dp[i] = dp[i + 1]
+        #         if i + 1 < n and 10 <= int(s[i:i+2]) <= 26:
+        #             dp[i] += dp[i + 2]
 
-        return dp[len(s)]
+        #     return dp[0] 
