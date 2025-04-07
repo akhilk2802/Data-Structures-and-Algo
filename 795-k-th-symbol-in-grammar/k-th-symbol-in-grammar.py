ad442d5ea1a8d21da1cc2395ac57ac7b4a9ade0f
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-04-07 12:44:08 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (84.64%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
@@ -1,10 +1,8 @@
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
 
-        # Optimal ->
         cur = 0
-        left = 0
-        right = 2 ** (n-1)
+        left, right = 1, 2**(n-1)
 
         for _ in range(n-1):
             mid = (left + right) // 2
@@ -13,9 +11,8 @@ class Solution:
             else:
                 left = mid + 1
                 cur = 0 if cur else 1
-
+        
         return cur
-
 
         
 
