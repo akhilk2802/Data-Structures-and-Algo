a58e3034f4769f91554639c64eee5fb2b0b594b8
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-04-07 01:02:58 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (62.94%) - LeetSync

--- 166-fraction-to-recurring-decimal/fraction-to-recurring-decimal.py
+++ 166-fraction-to-recurring-decimal/fraction-to-recurring-decimal.py
@@ -9,31 +9,48 @@ class Solution:
 
         if numerator == 0:
             return "0"
-
+        
         result = []
 
         if numerator * denominator < 0:
             result.append("-")
-
+        
         numerator, denominator = abs(numerator), abs(denominator)
+        
         result.append(str(numerator // denominator))
         remainder = numerator % denominator
+
         if remainder == 0:
             return "".join(result)
         
         result.append(".")
-        seen = {}
 
+        seen = {}
         while remainder:
             if remainder in seen:
                 result.insert(seen[remainder], "(")
                 result.append(")")
                 break
-            
-            seen[remainder] = len(result)
 
+            seen[remainder] = len(result)
             remainder *= 10
             result.append(str(remainder // denominator))
-            remainder %= denominator
+            remainder = remainder % denominator
         
         return "".join(result)
+
+        # seen = {}
+        # while remainder:
+        #     if remainder in seen:
+        #         result.insert(seen[remainder], "(")
+        #         result.append(")")
+        #         break
+
+        #     seen[remainder] = len(result)
+        #     remainder *= 10
+        #     result.append(str(remainder // denominator))
+        #     remainder = remainder % denominator
+        
+        # # print("result -> ", result)
+        # return "".join(result)
+
