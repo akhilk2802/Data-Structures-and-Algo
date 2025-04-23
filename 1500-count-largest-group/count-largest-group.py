9a6dea8adb8aa02af5c18bd96f772b47348c1e0f
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-04-23 17:45:10 -0400

    Time: 26 ms (71.78%) | Memory: 18.4 MB (7.79%) - LeetSync

---
+++ 1500-count-largest-group/count-largest-group.py
@@ -0,0 +1,29 @@
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        
+        def findSum(x):
+            total = 0
+            x = abs(x)
+            while x > 0:
+                total += x % 10
+                x //= 10
+            return total
+        
+        m = {}
+
+        for i in range(1, n + 1):
+
+            t = findSum(i)
+            if t in m:
+                m[t].append(i)
+            else:
+                m[t] = [i]
+
+        # print("Map -> ", m)
+        max_size = max(len(v) for v in m.values())
+        count = 0
+        for v in m.values():
+            if len(v) == max_size:
+                count += 1
+
+        return count
