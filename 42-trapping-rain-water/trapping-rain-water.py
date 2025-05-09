6bcb50387c33c8f14606247b7fde789bf19eb953
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-05-09 16:27:02 -0400

    Time: 3 ms (97.95%) | Memory: 19.1 MB (99.06%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,30 +1,25 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         
-        # algo - 
-        # 1. Start with two pointer
-        # 2. left starts from 0 and right from the last element 
-        # 3. maintain the max height from each side 
-        # 4. Keep shifting the pointers towards each other comparing with the height 
-        # 5. if i find any height higher than previous, i would replace existing and find the total water it can hold
-
-        l, r = 0, len(height) - 1
-        max_left = height[l]
-        max_right = height[r]
+        left, right = 0, len(height) - 1
+        max_left = height[left]
+        max_right = height[right]
         total = 0
 
-        while l < r:
-            if height[l] <= height[r]:
-                if height[l] > max_left:
-                    max_left = height[l]
+        while left < right:
+
+            if height[left] <= height[right]:
+                if height[left] > max_left:
+                    max_left = height[left]
                 else:
-                    total += (max_left - height[l])
-                l += 1
+                    total += (max_left - height[left])
+                left += 1
             else:
-                if height[r] > max_right:
-                    max_right = height[r]
+                if height[right] > max_right:
+                    max_right = height[right]
                 else:
-                    total += (max_right - height[r])
-                r -= 1
+                    total += (max_right - height[right])
+
+                right -= 1
 
         return total
