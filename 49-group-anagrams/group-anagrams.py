14b1cbac7ab254cb84597dedd28fa1ffeea6e2ea
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-05-09 14:40:39 -0400

    Time: 8 ms (92.96%) | Memory: 20.7 MB (76.59%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -6,12 +6,10 @@ class Solution:
         for c in strs:
             sorted_c = "".join(sorted(c))
 
-            # m.get(sorted_c, []).append(c)
-
-            if sorted_c in m:
-                m[sorted_c].append(c)
-            else:
+            if sorted_c not in m:
                 m[sorted_c] = [c]
+            else:
+                m[sorted_c].append(c)
 
-        # print("map : ", m)
-        return [c for c in m.values()]
+        # print("m : ", m)
+        return [v for v in m.values()]
