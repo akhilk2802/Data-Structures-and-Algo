9ebbc87d6232c062b8af14ccbc058a90a2e09782
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-05-09 20:14:24 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (84.88%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -6,12 +6,13 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 
-        head = ListNode(0)
-        tail = head
+        dummy = ListNode(0)
+        tail = dummy
+        sum_so_far = 0
         carry = 0
-        s = 0
 
         while l1 or l2 or carry:
+
             d1 = l1.val if l1 else 0
             d2 = l2.val if l2 else 0
 
@@ -26,6 +27,7 @@ class Solution:
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
         
-        result = head.next
-        head.next = None
+        result = dummy.next
+        # dummy.next = None
         return result
+
