4dd7305368aba631fc97d989dfce45871ed21798
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-05-20 17:13:28 -0400

    Time: 2 ms (7.17%) | Memory: 18 MB (12.56%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -7,20 +7,24 @@ class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
 
         if not head:
-            return None
+            return []
 
         pointer1, pointer2 = head, head
 
         for _ in range(n):
-            pointer2 = pointer2.next
+            pointer1 = pointer1.next
+        
+        if pointer1:
+            print("pointer1 value -> ", pointer1.val)
+            print("pointer2 value -> ", pointer2.val)
 
-        if not pointer2:
+        if not pointer1:
             return head.next
 
-        while pointer2.next:
+        while pointer1 and pointer1.next:
             pointer1 = pointer1.next
             pointer2 = pointer2.next
 
-        pointer1.next = pointer1.next.next
+        pointer2.next = pointer2.next.next
 
         return head
