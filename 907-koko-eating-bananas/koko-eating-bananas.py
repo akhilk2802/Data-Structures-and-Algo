c337bc99268c30ba39c1b5039209c77b40bbdb86
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-05-20 16:54:11 -0400

    Time: 186 ms (22.00%) | Memory: 19.2 MB (10.31%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,23 +1,30 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        """
+        I want to understand the thought process right, this is one of the craziest algorithms
+        """
+
+        def canFinish(value):
+
+            req_hour = 0
+            for i in range(len(piles)):
+                req_hour += math.ceil(piles[i] / value)
+            
+            return req_hour <= h
 
         low = 1
         high = max(piles)
 
-        while low < high:
-            
+        while low <= high:
+
             mid = (low + high) // 2
-
-            time = 0
-            for pile in piles:
-                time += (pile + mid - 1) // mid
-
-            if time <= h:
-                high = mid
+            if canFinish(mid):
+                high = mid - 1
             else:
                 low = mid + 1
-
+        
         return low
+
 
 
 
