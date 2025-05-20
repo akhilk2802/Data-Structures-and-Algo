467b25c26ea5711d1898d73996c78c5a0fefa906
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-05-20 17:02:58 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (61.59%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -11,9 +11,10 @@ class Solution:
 
         while list1 and list2:
 
-            if list1.val < list2.val:
+            if list1.val <= list2.val:
                 curr.next = list1
                 list1 = list1.next
+            
             else:
                 curr.next = list2
                 list2 = list2.next
@@ -23,4 +24,3 @@ class Solution:
             curr.next = list1 if list1 else list2
 
         return dummy.next
-        
