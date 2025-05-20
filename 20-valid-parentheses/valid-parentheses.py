23230bdb7b8ab140b23839eedaf30f7662dc6d64
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-05-20 15:38:37 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (80.83%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -4,14 +4,18 @@ class Solution:
         if not s:
             return False
 
-        d = {'(':')', '{':'}', '[':']'}
+        p_map = {
+            "(" : ")",
+            "{" : "}",
+            "[" : "]"
+        }
+
         stack = []
 
-        for i in s:
-            if i in d:
-                stack.append(i)
-            elif len(stack) == 0 or d[stack.pop()] != i:
+        for ch in s:
+            if ch in p_map:
+                stack.append(ch)
+            elif len(stack) == 0 or p_map[stack.pop()] != ch:
                 return False
-            
+
         return len(stack) == 0
-            
