c20f46c2007a4d3ec892b6ffbc11afdeb939cbbb
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-06-03 20:49:51 -0400

    Time: 2 ms (22.07%) | Memory: 18 MB (23.01%) - LeetSync

---
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+
+        
+        if not root:
+            return []
+        
+        c_map = {}
+
+        queue = deque([(root, 0)])
+
+        while queue:
+            node, col = queue.popleft()
+
+            if col in c_map:
+                c_map[col].append(node.val)
+            else:
+                c_map[col] = [node.val]
+
+            if node.left:
+                queue.append((node.left, col-1))
+            if node.right:
+                queue.append((node.right, col+1))
+
+
+        sort_map = sorted(c_map.items(), key=lambda i:i[0])
+        print("map -> ", sort_map)
+        result = []
+
+        for k, v in sort_map:
+            result.append(v)
+
+        return result
