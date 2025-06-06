dfda69969ba7172e1fac20a7c9dffece4e82f149
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-06-06 06:12:39 +0530

    Time: 79 ms (41.06%) | Memory: 21.8 MB (32.75%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,28 +1,62 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        if not s:
-            return 0
 
+        '''
+        s -> expression with digits and signs, NO Parantheses
+        Evalute and return value -> integer
+        given expression -> valid
+        BODMAS
+
+        Example ->
+        Input -> 3 + 2 * 2
+        Output -> 3 + 4 -> 7
+
+        Example 2 -> 
+        Input -> 3 / 2
+        Output -> 1
+
+        Example 3 ->
+        Input -> 3 + 5 / 2
+        Output -> 3 + 2 -> 5
+        '''
+
+        '''
+        Approach ->
+        1. Use a Stack
+        2. Iterate through the given expression
+        3. Every time we find theres an expression
+
+        3 + 2 * 2
+        '''
+
+        # Stack
         stack = []
-        num = 0
+
+        # Sign and Current Number
         sign = "+"
-        
+        num = 0
+
+        # Loop through the given string
         for i, char in enumerate(s):
             if char.isdigit():
                 num = num * 10 + int(char)
-
+            
+            
             if char in "+-/*" or i == len(s) - 1:
                 if sign == "+":
                     stack.append(num)
-                elif sign == "-":
+                if sign == "-":
                     stack.append(-num)
-                elif sign == "*":
-                    stack.append(stack.pop() * num)
-                elif sign == "/":
+                if sign == "/":
                     stack.append(int(stack.pop() / num))
-
+                if sign == "*":
+                    stack.append(stack.pop() * num)
+                
                 sign = char
                 num = 0
 
         return sum(stack)
-        
+
+
+        # based on the sign: take action
+
