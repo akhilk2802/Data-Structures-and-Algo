5b78d40b159da03437bdb89036f2f16f66a6c65f
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-06-06 06:54:48 +0530

    Time: 13 ms (15.89%) | Memory: 18.3 MB (47.46%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,9 +1,12 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        newStr = ''
+        
+        newStr = ""
 
         for i in range(len(s)):
+
             if s[i].isalnum():
                 newStr += s[i].lower()
 
+        print("newStr : ", newStr)
         return newStr == newStr[::-1]
