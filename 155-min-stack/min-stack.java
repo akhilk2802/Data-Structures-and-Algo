191254b84a8d6f444636ea15c282fdbd65269401
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-06-19 03:28:15 +0530

    Time: 4 ms (97.41%) | Memory: 44.7 MB (82.96%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,45 +1,40 @@
-
-
 import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
 
 class MinStack {
 
-    Stack<Integer> minStack;
-    Stack<Integer> stack;
+    List<Integer> minStack;
+    List<Integer> stack;
     
     public MinStack() {
 
-        minStack = new Stack<>();
-        stack = new Stack<>();
+        minStack = new ArrayList<>();
+        stack = new ArrayList<>();
 
     }
     
     public void push(int val) {
 
         stack.add(val);
-        if (minStack.size() == 0 || minStack.peek() >= val) {
+        if (minStack.size() == 0 || minStack.get(minStack.size() - 1) >= val) {
             minStack.add(val);
         }
     }
     
     public void pop() {
 
-        int popVal = stack.pop();
-        if (minStack.size() > 0 && minStack.peek().equals(popVal)) {
-            minStack.pop();
+        int popVal = stack.remove(stack.size() - 1);
+        if (minStack.size() > 0 && minStack.get(minStack.size() - 1).equals(popVal)) {
+            minStack.remove(minStack.size() - 1);
         }
     }
     
     public int top() {
-
-        return stack.peek();
+        return stack.get(stack.size() - 1);
         
     }
     
     public int getMin() {
-
-        return minStack.peek();
-        // return stack.peek();
+        return minStack.get(minStack.size() - 1);
         
     }
 }
