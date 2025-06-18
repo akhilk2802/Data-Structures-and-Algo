74130d44adb9ab1e76a926b58f7d1a9bceb379f8
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-06-19 04:43:52 +0530

    Time: 794 ms (7.26%) | Memory: 45.9 MB (7.47%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        char[] charsS1 = s1.toCharArray();
+        Arrays.sort(charsS1);
+        String sortedS1 = new String(charsS1);
+
+        int n = sortedS1.length(), m = s2.length();
+
+        for (int i = 0; i < m - n + 1; i++) {
+            
+            String partS2 = s2.substring(i, i + n);
+            char[] charsPartS2 = partS2.toCharArray();
+            Arrays.sort(charsPartS2);
+            String sortedPartS2 = new String(charsPartS2);
+
+            if(sortedS1.equals(sortedPartS2)){
+                return true;
+            }
+
+        }
+        return false;
+        
+        
+    }
+}
