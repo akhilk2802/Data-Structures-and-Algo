b5e29c5c95ba27353e993619cd92e7abc6adf18f
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-06-19 04:19:30 +0530

    Time: 30 ms (69.41%) | Memory: 51.6 MB (58.98%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -4,43 +4,40 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length - 2; i++){
+        for (int i = 0; i < nums.length - 2; i++ ) {
 
-            if (i > 0 && nums[i] == nums[i-1]){
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
-            int totalSum = -nums[i];
+            int total = -nums[i];
 
             int left = i + 1;
             int right = nums.length - 1;
 
-            while (left < right){
- 
-                int sumInternal = nums[left] + nums[right];
-                if (sumInternal == totalSum){
+            while (left < right) {
+                
+                int val = nums[left] + nums[right];
+                if (total == val) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                    List<Integer> v = new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right]));
-                    result.add(v);
-
-                    while ((left < right) && nums[left] == nums[left + 1]){
+                    while ((left < right) && nums[left] == nums[left + 1]) {
                         left++;
                     }
-                    while ((left < right) && nums[right] == nums[right - 1]){
+                    while ((left < right) && nums[right] == nums[right - 1]) {
                         right--;
                     }
 
                     left++;
                     right--;
-
-                } else if (sumInternal > totalSum) {
+                }
+                else if (val > total) {
                     right--;
-                } else {
+                }
+                else{
                     left++;
                 }
-
             }
-
         }
         return result;
     }
