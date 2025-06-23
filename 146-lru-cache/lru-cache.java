96edcfc80a6be28e7c6f562aa1ce435d57d75141
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-06-23 20:56:19 +0530

    Time: 49 ms (40.53%) | Memory: 127.5 MB (10.78%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -71,8 +71,8 @@ class LRUCache {
     public void insertInFront(Node node) {
 
         node.next = head.next;
-        node.prev = head;
         head.next.prev = node;
+        node.prev = head;
         head.next = node;
 
     }
