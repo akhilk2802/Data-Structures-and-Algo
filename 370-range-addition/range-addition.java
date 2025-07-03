b0c9413425a3677506a5a08b50d76099877a89b8
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-07-03 15:17:47 -0400

    Time: 2 ms (100.00%) | Memory: 49.9 MB (87.74%) - LeetSync

--- 370-range-addition/range-addition.java
+++ 370-range-addition/range-addition.java
@@ -13,18 +13,48 @@ class Solution {
         [-2,0,3,5,3]
         */
 
-        int[] arr = new int[length];
+        // int[] arr = new int[length];
 
-        for (int i = 0; i < updates.length; i++) {
-            int[] interval = updates[i];
-            int start = interval[0];
-            int end = interval[1];
-            int inc = interval[2];
+        // for (int i = 0; i < updates.length; i++) {
+        //     int[] interval = updates[i];
+        //     int start = interval[0];
+        //     int end = interval[1];
+        //     int inc = interval[2];
 
-            for (int j = start; j <= end; j++ ) {
-                arr[j] += inc;
+        //     for (int j = start; j <= end; j++ ) {
+        //         arr[j] += inc;
+        //     }
+        // }
+        // return arr;
+
+        int[] diffArray = new int[length + 1];
+
+        for (int[] arr : updates) {
+            int start = arr[0];
+            int end = arr[1];
+            int inc = arr[2];
+
+            diffArray[start] += inc;
+            diffArray[end + 1] -= inc;
+        }
+
+        int[] result = new int[length];
+
+        // diffarray => [2, 2, 3, 2, -2, -3]
+        // result = 
+
+        for (int i = 0; i < length; i++) {
+
+            if (i == 0) {
+                result[i] = 0 + diffArray[i];
+            }
+            else {
+                result[i] = result[i-1] + diffArray[i];
             }
         }
-        return arr;
+
+        return result;
+
+
     }
 }
