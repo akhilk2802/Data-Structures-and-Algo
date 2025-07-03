cab590c29de3348e37115469da49fabdb1b7921d
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-07-03 15:59:02 -0400

    Time: 1 ms (92.92%) | Memory: 45.2 MB (88.58%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -6,8 +6,9 @@ class Solution {
 
         int leftSum = 0;
         for (int i = 0; i < nums.length; i++) {
+            int rightSum = totalSum - leftSum - nums[i];
 
-            if (leftSum == (totalSum - leftSum - nums[i])) {
+            if (leftSum == rightSum) {
                 return i;
             }
             leftSum += nums[i];
