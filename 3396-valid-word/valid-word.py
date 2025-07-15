0773fc41e144e1831681b59028d627f068fb1b99
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-07-14 21:04:58 -0400

    Time: 3 ms (18.53%) | Memory: 17.7 MB (86.06%) - LeetSync

---
+++ 3396-valid-word/valid-word.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+
+        vowels = {'a', 'i', 'e', 'o', 'u'}
+
+        vowel_count = 0
+        consonant_count = 0
+
+        if len(word) < 3:
+            return False
+        
+        for ch in word: 
+            ch = ch.lower()
+            if ch.isalnum():
+                if ch.isalpha() and ch in vowels:
+                    vowel_count += 1
+                if ch.isalpha() and ch not in vowels:
+                    consonant_count += 1
+            else:
+                return False
+
+        print("con_count -> ", consonant_count)
+        print("vow_count -> ", vowel_count)
+                
+        if vowel_count >= 1 and consonant_count >= 1:
+            return True
+
+        return False
+
+
+        
