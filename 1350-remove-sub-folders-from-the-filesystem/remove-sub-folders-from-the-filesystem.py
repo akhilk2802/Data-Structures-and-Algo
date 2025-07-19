5841fc1de4a4931ad44a7b36dd4c6c397ffd34f6
Akhilesh S Kumbar <70321468+akhilk2802@users.noreply.github.com>
2025-07-19 03:17:48 -0400

    Time: 27 ms (75.88%) | Memory: 30.5 MB (95.49%) - LeetSync

---
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -0,0 +1,28 @@
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+
+        '''
+        Comeback later
+        '''
+
+
+
+
+
+        # Sort the folders alphabetically
+        folder.sort()
+
+        # Initialize the result list and add the first folder
+        result = [folder[0]]
+
+        # Iterate through each folder and check if it's a sub-folder of the last added folder in the result
+        for i in range(1, len(folder)):
+            last_folder = result[-1]
+            last_folder += "/"
+
+            # Check if the current folder starts with the last added folder path
+            if not folder[i].startswith(last_folder):
+                result.append(folder[i])
+
+        # Return the result containing only non-sub-folders
+        return result
